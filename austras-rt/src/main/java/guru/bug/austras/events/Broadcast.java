ed473d4c0977abf202352578b294643e7e36ced8
Dimitrijs Fedotovs <dima@fedoto.ws>
2019-08-19 22:23:23 +0300

    refactoring, starting functionality for broadcasting events

--- austras-rt/src/main/java/guru/bug/austras/events/Broadcast.java
+++ austras-rt/src/main/java/guru/bug/austras/events/Broadcast.java
@@ -1,5 +1,14 @@
 package guru.bug.austras.events;
 
+// TODO second type param for response type
 public interface Broadcast<M> {
+    // TODO response consumer as the second param (result of each called method must be passed to this consumer)
     void send(M message);
+
+    /**
+     * convenience method, the same as send(null). Useful when @Message annotation is placed on method, not on parameter
+     */
+    default void send() {
+        send(null);
+    }
 }
