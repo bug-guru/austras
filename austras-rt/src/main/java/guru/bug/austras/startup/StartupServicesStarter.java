3b1acbd96d5f0d5dea860c1b0345fc43be8382de
Dimitrijs Fedotovs <dima@fedoto.ws>
2019-09-07 21:29:13 +0300

    austras core: better termination logging

--- austras-rt/src/main/java/guru/bug/austras/startup/StartupServicesStarter.java
+++ austras-rt/src/main/java/guru/bug/austras/startup/StartupServicesStarter.java
@@ -23,18 +23,19 @@ public class StartupServicesStarter {
         Runtime.getRuntime().addShutdownHook(new Terminator());
         if (services == null || services.isEmpty()) {
             log.info("No services found. Nothing to initialize.");
-            return;
+        } else {
+            log.info(() -> format("Initializing %d services", services.size()));
+            services.forEach(this::init);
         }
-        services.forEach(this::init);
     }
 
     private void init(StartupService startupService) {
-        log.info(() -> format("Initializing service %s", startupService.getClass()));
+        log.info(() -> format("Initializing service %s", startupService.getClass().getName()));
         try {
             startupService.initialize();
             initializedStartupServices.add(startupService);
         } catch (Exception e) {
-            var msg = format("Error initializing service %s", startupService.getClass());
+            var msg = format("Error initializing service %s", startupService.getClass().getName());
             log.log(Level.SEVERE, msg, e);
             System.exit(1);
             throw new IllegalStateException(msg, e);
@@ -43,6 +44,7 @@ public class StartupServicesStarter {
 
     private class Terminator extends Thread {
 
+        // System out is being used because logging has its own shutdown hook and may be stopped before this.
         @Override
         public void run() {
             System.out.printf("Stopping %d services\n", initializedStartupServices.size());
@@ -52,12 +54,12 @@ public class StartupServicesStarter {
 
         private void destroy(StartupService startupService) {
             try {
-                System.out.printf("Stopping service %s\n", startupService.getClass());
+                System.out.printf("Stopping service %s\n", startupService.getClass().getName());
                 startupService.destroy();
             } catch (Exception e) {
                 var buf = new StringWriter();
                 var out = new PrintWriter(buf);
-                out.printf("Error destroying service %s: ", startupService.getClass());
+                out.printf("Error stopping service %s: ", startupService.getClass().getName());
                 e.printStackTrace(out);
                 out.flush();
                 out.close();
