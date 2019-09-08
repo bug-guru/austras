fea038d31826b3df01db26b9460c922b40a16a83
Dimitrijs Fedotovs <dima@fedoto.ws>
2019-09-08 10:46:56 +0300

    adding austras-web and austras-web-apt modules renaming core modules

--- austras-core-apt/src/main/java/guru/bug/austras/code/CompilationUnit.java
+++ austras-core-apt/src/main/java/guru/bug/austras/code/CompilationUnit.java
@@ -1,9 +1,9 @@
 package guru.bug.austras.code;
 
+import guru.bug.austras.code.common.QualifiedName;
 import guru.bug.austras.code.decl.ImportDecl;
 import guru.bug.austras.code.decl.PackageDecl;
 import guru.bug.austras.code.decl.TypeDecl;
-import guru.bug.austras.code.common.QualifiedName;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
