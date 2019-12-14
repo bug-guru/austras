8536f6154e12cef63e91a99cb943e213abaaa1a9
Dimitrijs Fedotovs <dima@fedoto.ws>
2019-12-15 01:33:28 +0200

    sync

--- austras-core/src/main/java/guru/bug/austras/core/Provider.java
+++ austras-core/src/main/java/guru/bug/austras/core/Provider.java
@@ -1,5 +1,11 @@
 package guru.bug.austras.core;
 
+import guru.bug.austras.meta.QualifierMetaInfo;
+
+import java.util.Set;
+
 public interface Provider<T> {
     T get();
+
+    Set<QualifierMetaInfo> qualifiers();
 }
--- austras-core/src/main/java/guru/bug/austras/meta/QualifierMetaInfo.java
+++ austras-core/src/main/java/guru/bug/austras/meta/QualifierMetaInfo.java
@@ -1,21 +1,37 @@
 package guru.bug.austras.meta;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class QualifierMetaInfo {
     private final String name;
     private final Map<String, String> properties;
 
-    public QualifierMetaInfo(String name, Map<String, String> properties) {
+    private QualifierMetaInfo(String name, Map<String, String> properties) {
         this.name = name;
         this.properties = properties == null ? Map.of() : Map.copyOf(properties);
     }
 
-    public static Builder builder() {
-        return new Builder();
+    public static Builder builderFor(String name) {
+        return new Builder(name);
     }
 
-    public static class Builder {
 
+    public static class Builder {
+        private final String name;
+        private final Map<String, String> properties = new HashMap<>();
+
+        private Builder(String name) {
+            this.name = name;
+        }
+
+        public Builder property(String key, String value) {
+            properties.put(key, value);
+            return this;
+        }
+
+        public QualifierMetaInfo build() {
+            return new QualifierMetaInfo(name, properties);
+        }
     }
 }
