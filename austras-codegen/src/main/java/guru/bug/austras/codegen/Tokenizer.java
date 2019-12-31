f2d7e79c798c257d47fde171fb22228e79a410f6
Dimitrijs Fedotovs <dima@fedoto.ws>
2019-12-31 19:03:30 +0200

    adding copyright info

--- austras-codegen/src/main/java/guru/bug/austras/codegen/Tokenizer.java
+++ austras-codegen/src/main/java/guru/bug/austras/codegen/Tokenizer.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019 Dimitrijs Fedotovs
+ * This software is licensed under the terms of the MIT license
+ * See LICENSE for the license details.
+ *
+ */
+
 package guru.bug.austras.codegen;
 
 import java.util.ArrayList;
--- austras-test/src/main/java/guru/bug/austras/test/chain/CompChain0.java
+++ austras-test/src/main/java/guru/bug/austras/test/chain/CompChain0.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019 Dimitrijs Fedotovs
+ * This software is licensed under the terms of the MIT license
+ * See LICENSE for the license details.
+ *
+ */
+
 package guru.bug.austras.test.chain;
 
 public class CompChain0 { //NOSONAR for testing purposes only
