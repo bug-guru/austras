d0da64d991682767a22759cc6f517993cbab44a3
Dimitrijs Fedotovs <dima@fedoto.ws>
2020-01-18 00:16:41 +0200

    sync new generator

--- austras-codegen/src/main/java/guru/bug/austras/codegen/generator/FileGenerator.java
+++ austras-codegen/src/main/java/guru/bug/austras/codegen/generator/FileGenerator.java
@@ -10,18 +10,30 @@ package guru.bug.austras.codegen.generator;
 import guru.bug.austras.codegen.Template;
 
 import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 public abstract class FileGenerator {
+    private static final Map<Class<?>, TemplateHolder> templates = new ConcurrentHashMap<>();
 
     public FileGenerator() {
-        var templates = new ArrayList<TemplateHolder>();
+        var toCheck = new LinkedList<Class<?>>();
         Class<?> cur = getClass();
         while (!cur.equals(FileGenerator.class)) {
-
+            toCheck.addFirst(cur);
             cur = cur.getSuperclass();
         }
+        var ref = new MutableRef<TemplateHolder>();
+        for (Class<?> cls : toCheck) {
+            var holder = templates.computeIfAbsent(cls, c -> this.createTemplateHolder(c, ref.value));
+            ref.value = holder;
+        }
+    }
+
+    private TemplateHolder createTemplateHolder(Class<?> cls, TemplateHolder parent) {
+
     }
 
     protected void generate(PrintWriter out) {
@@ -30,5 +42,9 @@ public abstract class FileGenerator {
 
     private static class TemplateHolder {
         final
+    }
+
+    private static class MutableRef<T> {
+        T value;
     }
 }
