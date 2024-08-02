1082040d7faf12c295f054823da926c166ffb549
Dimitrijs Fedotovs <dima@fedoto.ws>
2024-08-02 15:44:13 +0300

    copyrights

--- austras-test/src/main/java/guru/bug/austras/test/broadcast/ReceiverVoid2Component.java
+++ austras-test/src/main/java/guru/bug/austras/test/broadcast/ReceiverVoid2Component.java
@@ -2,7 +2,6 @@
  * Copyright (c) 2019-2024 Dimitrijs Fedotovs
  * This software is licensed under the terms of the MIT license
  * See LICENSE for the license details.
- *
  */
 
 package guru.bug.austras.test.broadcast;
